37b00fabc435d4304163f633c321702016fb9959
William Klancnik <willk@Williams-MacBook-Pro.local>
2024-03-09 13:43:30 -0800

    Add home-manager installation

--- setup.sh
+++ setup.sh
@@ -4,11 +4,24 @@ if ! command -v nix-env --version &> /dev/null
 then
     if [ "$CODESPACES" == "true" ]
     then
+        echo "Installing nix in single user mode..."
         sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
     else
+        echo "Installing nix in multi-user mode..."
         sh <(curl -L https://nixos.org/nix/install)
         echo "Re-run this script from a new shell..."
         exit 0
     fi
+else
+    echo "Nix already installed."
 fi
 
+if ! [ -f /Users/$USER/.config/home-manager/home.nix ]
+then
+    echo "Installing home-manager..."
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update
+    nix-shell '<home-manager>' -A install
+else
+    echo "Home-manager already installed."
+fi
