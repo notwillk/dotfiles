84826e42064023eadfa7e4fb4ce13a75adb52193
William Klancnik <willk@Williams-MacBook-Pro.local>
2024-03-09 15:29:13 -0800

    Add gitconfig installation for codespaces

--- codespaces_setup.sh
+++ codespaces_setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-echo "TBD: setup dotfiles for codespaces"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Setup dotfiles for codespaces..."
+
+echo "Installing gitconfig..."
+cp "$SCRIPT_DIR/gitconfig" "$HOME/.gitconfig"
+
+echo "Done."
