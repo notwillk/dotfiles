688ae361333c09b1a98ff77407a4807bc5615755
William Klancnik <william@klancnik.com>
2024-03-23 14:52:52 -0700

    Add home-manager and sops

--- flake.nix
+++ flake.nix
@@ -5,16 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
           vim
-          hello
+          sops
+          age
         ];
 
       # Auto upgrade nix package and the daemon service.
@@ -43,7 +46,14 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#xxx
     darwinConfigurations."xxx" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
